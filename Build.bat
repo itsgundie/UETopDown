c230df793dbbed37663033c58cf10585f5e72569
Alexandr Ostrovskiy <a.ostrovskiy@rj-games.com>
2021-11-12 00:28:35 +0300

    all project files
    ready for CI build

---
+++ Build.bat
@@ -0,0 +1,6 @@
+"C:\UE_4.27\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project=C:\Users\Gundie\Desktop\Unreal\JenkinsTopDown\JenkinsTopDown.uproject ^
+-platform=Win64 -clientconfig=Development ^
+-archiveddirectory=C:\Users\Gundie\Desktop\Unreal\JenkinsTopDown\BuildFromBat ^
+-cook -stage -archive -pak -package -allmaps
